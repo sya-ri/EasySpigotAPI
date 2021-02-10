2a56ca302caaa9dfa508bb38ed3863edbb949fd2
sya-ri <sya79lua@gmail.com>
2021-02-10 20:45:14 +0900

    api(util/uuid): UUIDPlayer, UUIDEntity を追加

---
+++ api/src/main/kotlin/com/github/syari/spigot/api/util/uuid/UUIDEntity.kt
@@ -0,0 +1,46 @@
+package com.github.syari.spigot.api.util.uuid
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Entity
+import java.util.UUID
+
+/**
+ * [Entity] が持つ [UUID] を操作しやすくしたクラス
+ *
+ * 変数に保存する際には [Entity] ではなく [UUID] で保存することを推奨する
+ * @since 1.1.0
+ */
+data class UUIDEntity(val uniqueId: UUID) : Comparable<UUIDEntity> {
+    /**
+     * [Entity] として取得する
+     * @since 1.1.0
+     */
+    val entity: Entity?
+        get() = Bukkit.getEntity(uniqueId)
+
+    /**
+     * [UUIDEntity.uniqueId] を文字列として取得する
+     * @see [UUID.toString]
+     * @since 1.1.0
+     */
+    override fun toString() = uniqueId.toString()
+
+    /**
+     * [UUIDEntity.uniqueId] を比較する
+     * @see [UUID.compareTo]
+     * @since 1.1.0
+     */
+    override fun compareTo(other: UUIDEntity) = uniqueId.compareTo(other.uniqueId)
+
+    companion object {
+        /**
+         * [Entity] を [UUIDEntity] に変換する
+         */
+        fun from(entity: Entity) = UUIDEntity(entity.uniqueId)
+
+        /**
+         * [UUID] を表す文字列から [UUIDEntity] に変換する
+         */
+        fun from(uniqueId: String) = uuidOrNull(uniqueId)?.let(::UUIDEntity)
+    }
+}
