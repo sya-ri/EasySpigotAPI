0ca1e001dc565fb46f15cab14e8315318126db73
sya-ri <sya79lua@gmail.com>
2021-02-12 01:07:49 +0900

    sample: plugin-yml の内容を統一化 依存プラグインにEasySpigotAPIが追加されていないバグを修正

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 tasks.withType<Jar> {
     enabled = false
@@ -13,5 +14,16 @@ subprojects {
 
     tasks.withType<ShadowJar> {
         archiveBaseName.set("EasySpigotAPI-Sample-${project.name}")
+    }
+
+    configure<BukkitPluginDescription> {
+        name = "EasySpigotAPI-Sample-${project.name}"
+        version = project(":api").version.toString()
+        description = "EasySpigotAPI Sample: ${project.name}"
+        main = "com.github.syari.spigot.api.sample.${project.name.replace('-', '.')}.Main"
+        author = "sya_ri"
+        website = "https://github.com/sya-ri/EasySpigotAPI"
+        apiVersion = "1.16"
+        depend = listOf("EasySpigotAPI")
     }
 }
--- sample/scheduler/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-bukkit {
-    name = "EasySpigotAPI-Sample-scheduler"
-    version = project.version.toString()
-    description = "EasySpigotAPI Sample: scheduler"
-    main = "com.github.syari.spigot.api.sample.scheduler.Main"
-    author = "sya_ri"
-    website = "https://github.com/sya-ri/EasySpigotAPI"
-    apiVersion = "1.16"
-}
