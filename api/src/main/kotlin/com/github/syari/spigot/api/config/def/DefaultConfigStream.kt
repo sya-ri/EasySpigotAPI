9fb0db6325da3e23df71f94a34283ff790064d24
sya-ri <sya79lua@gmail.com>
2021-03-16 04:26:28 +0900

    config: DefaultConfigStream を追加

--- api/src/main/kotlin/com/github/syari/spigot/api/config/def/DefaultConfigStream.kt
+++ api/src/main/kotlin/com/github/syari/spigot/api/config/def/DefaultConfigStream.kt
@@ -1,18 +1,22 @@
 package com.github.syari.spigot.api.config.def
 
 import com.github.syari.spigot.api.config.CustomConfig
-import org.bukkit.plugin.java.JavaPlugin
+import java.io.InputStream
 
 /**
  * デフォルト設定として [CustomConfig] に対して処理を実行する。
  * @since 2.2.1
  */
-class DefaultConfigResource(val plugin: JavaPlugin, val fileName: String) : DefaultConfig {
+class DefaultConfigStream(val inputStream: InputStream) : DefaultConfig {
     /**
      * デフォルト設定を適用する。
      * @since 2.2.1
      */
     override fun set(config: CustomConfig) {
-        plugin.getResource(fileName)?.let(::DefaultConfigStream)?.set(config)
+        inputStream.use { input ->
+            config.file.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
     }
 }
