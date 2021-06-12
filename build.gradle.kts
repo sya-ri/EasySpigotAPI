e418ab88e0495d629c2bfaac438649cf3bab789c
sya-ri <sya79lua@gmail.com>
2021-06-12 23:06:28 +0900

    chore: 依存関係のバージョンを更新 - Kotlin 1.5.0 -> 1.5.10 - ktlint-gradle 10.0.0 -> 10.1.0 - ktlint 0.40 -> 0.41.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
-    kotlin("jvm") version "1.5.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    kotlin("jvm") version "1.5.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
     id("net.minecrell.plugin-yml.bukkit") version "0.4.0" apply false
 }
@@ -13,10 +12,6 @@ allprojects {
 
     repositories {
         mavenCentral()
-    }
-
-    configure<KtlintExtension> {
-        version.set("0.40.0")
     }
 }
 
